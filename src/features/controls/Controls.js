20700619b42a4b91e529766ab7c4ca194296cc1f
Hayoun Lee <sidhlee@gmail.com>
2020-02-06 22:33:07 +0900

    Start Controls

--- src/features/controls/Controls.js
+++ src/features/controls/Controls.js
@@ -1,5 +1,38 @@
 import React from "react";
+import {
+  FaPlay,
+  FaPause,
+  FaBackward,
+  FaForward
+} from "react-icons/fa";
 
 export default props => {
-  return <div>Controls</div>;
+  return (
+    <div>
+      {props.isPlaying ? (
+        <IconButton>
+          <FaPause />
+        </IconButton>
+      ) : (
+        <IconButton>
+          <FaPlay />
+        </IconButton>
+      )}
+      <SpaceGif width="10px" />
+      <IconButton>
+        <FaBackward />
+      </IconButton>
+      <IconButton>
+        <FaForward />
+      </IconButton>
+    </div>
+  );
 };
+
+function IconButton(props) {
+  return <button {...props} />;
+}
+
+function SpaceGif({ width }) {
+  return <div style={{ display: "inline-block", width }} />;
+}
