d441778e0c0a902990f31a8a94cfb2a4a1651273
Hayoun Lee <sidhlee@gmail.com>
2020-02-06 20:34:34 +0900

    Initial commit
    setup basic structure + assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Slides from "./features/slides/Slides";
+import SlideNav from "./features/slideNav/SlideNav";
+import Controls from "./features/controls/Controls";
+import ProgressBar from "./features/progressBar/ProgressBar";
+
+function Carousel(props) {
+  return <section className="Carousel" {...props} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel>
+      <Slides />
+      <SlideNav />
+      <Controls />
+      <ProgressBar />
+    </Carousel>
   );
 }
 
---
+++ src/features/slides/images/index.js
@@ -0,0 +1,40 @@
+const metaImages = [
+  {
+    id: "img0",
+    title: "Brother and Sister",
+    content:
+      "Ethan and Elaine are watching Pokemon together on a bunker bed.",
+    position: "top"
+  },
+  {
+    id: "img1",
+    title: "My Ball!",
+    content:
+      "Dad and Ethan are kicking some balls at Youido Park under the blue sky.",
+    position: "top"
+  },
+  {
+    id: "img2",
+    title: "Sunshine Girl",
+    content: "Elaine strolling down in her pretty shoes.",
+    position: "bottom"
+  },
+  {
+    id: "img3",
+    title: "Here Goes The Cannon Ball!",
+    content: "Ethan is ready to launch his final attack on dad.",
+    position: "top"
+  },
+  {
+    id: "img4",
+    title: "Side by Side",
+    content: "A sunny afternoon at Han-River Park",
+    position: "bottom"
+  },
+  {
+    id: "img5",
+    title: "Good Morning Yellow Brick Road",
+    content: "Ethan is walking to the Kindergarten with mom and dad.",
+    position: "top"
+  }
+];
