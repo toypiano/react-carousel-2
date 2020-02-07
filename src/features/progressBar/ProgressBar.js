ed5b6254e45bb9edd71a11327a817991c50f2b56
Hayoun Lee <sidhlee@gmail.com>
2020-02-07 20:17:32 +0900

    Add ProgressBar UI

--- src/features/progressBar/ProgressBar.js
+++ src/features/progressBar/ProgressBar.js
@@ -1,5 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledProgressBar = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  & > div {
+    height: 20px;
+    background-color: hsla(0, 100%, 100%, 0.5);
+  }
+`;
 
 export default props => {
-  return <div>ProgressBar</div>;
+  const progress = 0.7;
+  return (
+    <StyledProgressBar>
+      <div style={{ width: `${progress * 100}%` }} />
+    </StyledProgressBar>
+  );
 };
