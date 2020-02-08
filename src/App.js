88b879eeef091e86291c07695fc3ede198bee69e
Hayoun Lee <sidhlee@gmail.com>
2020-02-08 20:06:18 +0900

    Implment reposive slide width for xl screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-
+import styled from "styled-components";
 import { StateProvider } from "./store/store";
+
+import vars from "./common/vars";
 
 import Slides from "./features/slides/Slides";
 import SlideNav from "./features/slideNav/SlideNav";
 import Controls from "./features/controls/Controls";
 import ProgressBar from "./features/progressBar/ProgressBar";
 
+const StyledCarousel = styled.section`
+  @media (min-width: ${vars.xl}) {
+    position: relative;
+    max-width: 990px;
+    height: 100vh;
+    margin: auto;
+    display: flex;
+  }
+`;
 function Carousel(props) {
-  return <section className="Carousel" {...props} />;
+  return <StyledCarousel {...props} />;
 }
 
 function App() {
